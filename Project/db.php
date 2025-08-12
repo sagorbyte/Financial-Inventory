f8a4207ba8e263a10e3a989ed97a0b0151976eef
Sagor Majumder <sagormajumder777@gmail.com>
2025-08-12 21:54:43 +0600

    Change db,php

--- Project/db.php
+++ Project/db.php
@@ -15,3 +15,5 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
 function bdt($n){ return '৳' . number_format((float)$n, 0); } 
 function set_flash($m,$t='success'){ $_SESSION['_flash']=['m'=>$m,'t'=>$t]; }
 function get_flash(){ if(!empty($_SESSION['_flash'])){ $f=$_SESSION['_flash']; unset($_SESSION['_flash']); return $f;} return null; }
+
+?>
